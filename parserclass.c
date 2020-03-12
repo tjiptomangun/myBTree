be1bc0573d19b03b6730a71c15e1aee241cee948
hanky.acbb <hanky.acbb@telogic.com.sg>
2020-03-12 18:07:48 +0700

    synch

--- parserclass.c
+++ parserclass.c
@@ -77,43 +77,63 @@ static int __class_delete (PCLASS p)
 static int __class_printattributes(PCLASS p, int ident)
 {
 	print_identation (ident);
-	fprintf (stdout, "class.name : %s", p->name);
+	if (p->name[0])
+		fprintf (stdout, "class.name : %s", p->name);
+	else
+		fprintf (stdout, "class.name : ");
 	fprintf (stdout, "\n");
 	return 0;
 }
 
-PCLASS newclass (char *name)
-{
-	PCLASS pclass = (PCLASS) calloc (1, sizeof (CLASS));
+void __class_ctor (PCLASS pclass,char *name)
+{ 
 	pclass->this = pclass;
 	pclass->type = CLASS_CLASS;
 	strncpy (pclass->name, name, MAX_NAME_LENGTH); 
 	pclass->printattributes = __class_printattributes; 
 	pclass->delete = __class_delete; 
-	return pclass;	
+}
+
+PCLASS newclass (char *name)
+{
+	PCLASS pclass = (PCLASS) calloc (1, sizeof (CLASS));
+	if (pclass)
+		__class_ctor (pclass, name);
+	return pclass; 
 } 
 
 static int __l_item_printattributes(PL_ITEM p, int ident)
 {
 	return __class_printattributes(&p->class, ident); 
 }
+/**
+ * Name		: __l_item_delete 
+ * Description	: override __class_delete for ITEM structure
+ * Returns	
+ *		0 on success
+ */
 static int __l_item_delete(PL_ITEM p)
 {
 	p->next = NULL;
-	__class_delete(& p->class);
-	return 0;
+	return __class_delete(& p->class);
 }
-PL_ITEM newl_item (char *name)
+
+void __l_item_ctor (PL_ITEM pl_item, char *name)
 {
-	PL_ITEM pl_item = (PL_ITEM) calloc (1, sizeof (L_ITEM));
-	pl_item->class.this = (PCLASS) pl_item;
+	__class_ctor (&pl_item->class, name); 
 	pl_item->class.type = CLASS_L_ITEM; 
-	strncpy (pl_item->class.name, name, MAX_NAME_LENGTH);
 	pl_item->next = 0;
 	pl_item->class.printattributes = 
 		(int (*) (PCLASS, int))(__l_item_printattributes);
 	pl_item->class.delete = 
 		(int (*) (PCLASS))(__l_item_delete);
+}
+
+PL_ITEM newl_item (char *name)
+{
+	PL_ITEM pl_item = (PL_ITEM) calloc (1, sizeof (L_ITEM));
+	if (pl_item)
+		__l_item_ctor (pl_item, name);
 	return pl_item;
 }
 
@@ -133,6 +153,36 @@ static int __list_printattributes(PLIST p, int ident)
 }
 
 /**
+ * NAME		: __list_addproperty
+ * DESCRIPTION	: add new property
+ * INPUT
+ *		- plist , this pointer
+ *		- name , name of this property
+ *		- value, value of this property
+ **/
+static int  __list_addproperty (PLIST plist, char *name, char *value)
+{
+	PPROPERTY add = newproperty2 (name, value);
+	if (!add)
+		return -1; 
+	if (plist->head == NULL)
+	{
+		plist->head = &add->l_item;
+		plist->tail = plist->head; 
+		plist->currptr = plist->head;
+		plist->count++; 
+	}
+	else
+	{
+		plist->tail->next = &add->l_item;
+		plist->tail = &add->l_item; 
+		plist->count++;
+	}
+	plist->tail->next = NULL;
+	return 0;
+}
+
+/**
  * NAME		: __list_add
  * DESCRIPTION	: static function that add time to list class 
  */
@@ -142,6 +192,7 @@ static void __list_add (PLIST plist, PL_ITEM add)
 	{
 		plist->head = add;
 		plist->tail = plist->head; 
+		plist->currptr = plist->head;
 		plist->count++;
 	}
 	else
@@ -152,6 +203,7 @@ static void __list_add (PLIST plist, PL_ITEM add)
 	}
 	plist->tail->next = NULL;
 }
+
 /**
  * NAME		: __list_detach
  * DESCRIPTION	: detach a specified list item
@@ -164,8 +216,11 @@ static PL_ITEM  __list_detach (PLIST plist, PL_ITEM detached)
 	PL_ITEM curr = plist->head;
 	while (curr)
 	{
-		if (detached == curr)
+		if (detached == curr){
+			if (detached == plist->currptr)
+				plist->currptr = plist->currptr->next;
 			return NULL;
+		}
 		curr = curr->next;
 	} 
 	return detached;
@@ -205,8 +260,7 @@ static int __list_delete(PLIST plist)
 		curr->class.delete (&curr->class);
 		plist->count --;
 	}
-	__class_delete (&plist->l_item.class);
-	return 0;
+	return __class_delete (&plist->l_item.class);
 }
 
 /**
@@ -248,24 +302,13 @@ static PL_ITEM __list_takename(PLIST plist, char* name)
  */
 static PL_ITEM __list_getname(PLIST plist, char* name)
 {
-	PL_ITEM curr = plist->head;
-	PL_ITEM prev = NULL;
-		
+	PL_ITEM curr = plist->head; 
 	while (curr)
 	{ 
 		if (!strncmp (name, curr->class.name, MAX_NAME_LENGTH))
 		{
-			if (prev)
-			{
-				prev->next = curr->next; 
-			}
-			else
-			{
-				plist->head = curr->next; 
-			}
 			return curr;
 		}
-		prev = curr;	
 		curr = curr->next;
 	} 
 	return NULL;
@@ -297,68 +340,64 @@ static PL_ITEM __list_getnextchild (PLIST plist)
 	return ret;	
 }
 
-PLIST newlist (char *list_name)
-{
-	PLIST plist = (PLIST) calloc (1, sizeof (LIST));
-	if (plist)
-	{
-		plist->l_item.class.this = (PCLASS) plist;
-		plist->l_item.class.type = CLASS_LIST; 
-		plist->currptr = NULL;
-		strncpy (plist->l_item.class.name, list_name, MAX_NAME_LENGTH - 1); 
-		plist->l_item.next = NULL;
-		plist->count = 0;
-		plist->add = __list_add;
-		plist->take = __list_take;
-		plist->takename = __list_takename; 
-		plist->getname = __list_getname; 
-		plist->getfirstchild = __list_getfirstchild; 
-		plist->getnextchild= __list_getnextchild; 
-		plist->l_item.class.printattributes = 
-			(int(*)(PCLASS, int))(__list_printattributes);
-		plist->l_item.class.delete = 
-			(int(*)(PCLASS))(__list_delete); 
-		plist->detach = __list_detach;
-			
-	}
-	return plist;
+void __list_ctor (PLIST plist, char *list_name)
+{ 
+	__l_item_ctor (&plist->l_item, list_name);
+	plist->l_item.class.type = CLASS_LIST; 
+	plist->currptr = NULL; 
+	plist->l_item.next = NULL;
+	plist->count = 0;
+	plist->add = __list_add;
+	plist->take = __list_take;
+	plist->takename = __list_takename; 
+	plist->getname = __list_getname; 
+	plist->getfirstchild = __list_getfirstchild; 
+	plist->getnextchild= __list_getnextchild; 
+	plist->l_item.class.printattributes = 
+		(int(*)(PCLASS, int))(__list_printattributes);
+	plist->l_item.class.delete= 
+		(int(*)(PCLASS))(__list_delete); 
+	plist->detach = __list_detach; 
+	plist->addproperty = __list_addproperty;
 }
 
 void list_resetlist (PLIST plist, char *list_name)
 { 
-
-	if (plist)
-	{
-		plist->l_item.class.this = (PCLASS) plist;
-		plist->l_item.class.type = CLASS_LIST; 
-		plist->currptr = NULL;
-		strncpy (plist->l_item.class.name, list_name, MAX_NAME_LENGTH - 1); 
-		plist->l_item.next = NULL;
-		plist->count = 0;
-		plist->add = __list_add;
-		plist->take = __list_take;
-		plist->takename = __list_takename; 
-		plist->getname = __list_getname; 
-		plist->l_item.class.printattributes = 
-			(int(*)(PCLASS, int))(__list_printattributes);
-		plist->l_item.class.delete= 
-			(int(*)(PCLASS))(__list_delete); 
-	} 
-
+    __list_ctor(plist, list_name);
 }
+
+
+PLIST newlist (char *list_name)
+{
+	PLIST plist = (PLIST) calloc (1, sizeof (LIST));
+	list_resetlist(plist, list_name);
+	return plist;
+}
+
 
 static int __property_delete(PPROPERTY p) 
 {
-	memset (p->value, 0, 256);
+	memset (p->value, 0, sizeof(p->value));
 	__class_delete (&p->l_item.class);
 	return 0;
 }
 
 static int __property_setvalue (PPROPERTY p, char *value)
 {
-	strncpy (p->value, value, 256);
+	strncpy (p->value, value, sizeof(p->value));
 	return 0;
 }
+
+static int __property_getvalue (PPROPERTY p, char *value)
+{
+	strncpy (value, p->value, sizeof(p->value));
+	return 0;
+}
+
+static char* __property_getvalue_ptr (PPROPERTY p) {
+	return p->value;
+}
+
 static int __property_printattributes(PPROPERTY p, int ident)
 {
 	__l_item_printattributes(&p->l_item, ident);	
@@ -368,22 +407,73 @@ static int __property_printattributes(PPROPERTY p, int ident)
 	return 0;
 }
 
+/**
+ * NAME		: __property_ctor 
+ * DESCRIPTION	: Constructor for propery
+ * INPUT	  
+ *		- prop, self pointer
+ *		- name, object name
+ */
+void __property_ctor (PPROPERTY prop, char *name)
+{
+	__l_item_ctor (&prop->l_item, name);
+	prop->l_item.class.this = (PCLASS) prop;
+	prop->l_item.class.type = CLASS_PROPERTY;
+	strncpy (prop->l_item.class.name, name, MAX_NAME_LENGTH - 1);
+	memset (prop->value, 0, 256);
+	prop->setvalue = __property_setvalue;
+	prop->getvalue_ptr = __property_getvalue_ptr;
+	prop->getvalue= __property_getvalue;
+	prop->l_item.next = NULL;
+	prop->l_item.class.printattributes = 
+		(int (*) (PCLASS, int))(__property_printattributes);
+	prop->l_item.class.delete = 
+		(int (*) (PCLASS))(__property_delete);
+}
+
+/**
+ * NAME		: __property_ctor2 
+ * DESCRIPTION	: Constructor for propery
+ * INPUT	  
+ *		- prop, self pointer
+ *		- name, object name
+ *		- value, object value
+ */
+void __property_ctor2 (PPROPERTY prop, char *name, char *value)
+{ 
+	__l_item_ctor (&prop->l_item, name);
+	prop->l_item.class.this = (PCLASS) prop;
+	prop->l_item.class.type = CLASS_PROPERTY;
+	strncpy (prop->l_item.class.name, name, MAX_NAME_LENGTH - 1);
+	memset (prop->value, 0, 256);
+	prop->setvalue = __property_setvalue;
+	prop->getvalue_ptr = __property_getvalue_ptr;
+	prop->getvalue= __property_getvalue;
+	prop->l_item.next = NULL;
+	__property_setvalue (prop, value);
+	prop->l_item.class.printattributes = 
+		(int (*) (PCLASS, int))(__property_printattributes);
+	prop->l_item.class.delete = 
+		(int (*) (PCLASS))(__property_delete);
+}
+
 PPROPERTY newproperty (char *name)
 {
 	PPROPERTY prop = (PPROPERTY) calloc (1, sizeof (PROPERTY));
 	if (prop)
 	{	
-		prop->l_item.class.this = (PCLASS) prop;
-		prop->l_item.class.type = CLASS_PROPERTY;
-		strncpy (prop->l_item.class.name, name, MAX_NAME_LENGTH - 1);
-		memset (prop->value, 0, 256);
-		prop->setvalue = __property_setvalue;
-		prop->l_item.next = NULL;
-		prop->l_item.class.printattributes = 
-			(int (*) (PCLASS, int))(__property_printattributes);
-		prop->l_item.class.delete = 
-			(int (*) (PCLASS))(__property_delete);
+		__property_ctor (prop, name);
+	}
+	return prop;
+}
 
+PPROPERTY newproperty2 (char *name, char *value)
+{
+	
+	PPROPERTY prop = (PPROPERTY) calloc (1, sizeof (PROPERTY));
+	if (prop)
+	{	
+		__property_ctor2 (prop, name, value);
 	}
 	return prop;
 }
@@ -412,6 +502,31 @@ void * __stack_ptr_pop (PSTACK_PTR p)
 	return ret;
 }
 
+/**
+ * Name		: __stack_ptr_cleanup
+ * Description	: release all pointers in this stack
+ **/
+int  __stack_ptr_cleanup (PSTACK_PTR p)
+{
+	void * ptr;
+	while (p->top >= 0)
+	{
+		ptr = __stack_ptr_pop (p);
+		if (!ptr)
+			continue;
+		free (ptr);
+	}
+	return __stack_ptr_init (p);
+
+} 
+
+int __stack_ptr_isempty (PSTACK_PTR p)
+{
+	int ret = 0;
+	ret = (p->top < 0)? 1: 0;
+	return ret;
+}
+
 PSTACK_PTR newstackptr ()
 {
 	PSTACK_PTR p = (PSTACK_PTR) calloc (1, sizeof (STACK_PTR));
@@ -419,6 +534,8 @@ PSTACK_PTR newstackptr ()
 	p->init = __stack_ptr_init;
 	p->push = __stack_ptr_push;
 	p->pop = __stack_ptr_pop;
+	p->cleanup = __stack_ptr_cleanup;
+	p->is_empty = __stack_ptr_isempty;
 	return p;	
 }
  
@@ -714,8 +831,10 @@ static struct tree_item * __treeitem_getparent (struct tree_item *root)
 static struct tree_item * __treeitem_getfirstchild (struct tree_item *root)
 {
 	struct tree_item *ret=root->head;
-	if (root->head)
+	if (root->head){
+    root->curr = root->head;
 		root->curr = (PTREE_ITEM) root->curr->next;
+	}
 	return ret;
 }
 
@@ -837,26 +956,48 @@ static PTREE_ITEM __treeitem_takename (PTREE_ITEM root, char *name)
 static PTREE_ITEM __treeitem_getname (PTREE_ITEM root, char *name)
 {
 	PTREE_ITEM curr = root->head;
-	PTREE_ITEM prev = NULL;
 
 	while (curr)
 	{
 		if (!strncmp (name, curr->list.l_item.class.name, MAX_NAME_LENGTH))
 		{
-			if (prev)
-			{
-				prev->next = curr->next; 
-			}
-			else
-			{
-				root->head = curr->next;
-			}
 			return curr;
 		}
-		prev = curr;
 		curr = curr->next;
 	}
 	return NULL;
+}
+
+static int __treeitem_printattributes(PTREE_ITEM root, int ident){
+	__list_printattributes(&root->list, ident + 1);
+	PTREE_ITEM curr = root->head;
+	while(curr){
+		__treeitem_printattributes(curr, ident + 1);
+		curr = curr->next;
+	}
+	return 0;
+}
+
+
+void __treeitem_ctor (PTREE_ITEM  new, PTREE_ITEM parent, char *name)
+{
+	__list_ctor (&new->list, name);
+	new->head = new->tail = new->curr = new->next = 0;
+	new->parent = parent;
+	list_resetlist (&new->list, name);
+	new->list.l_item.class.delete = (int  (*) (PCLASS) ) (__treeitem_listdelete);
+	new->add = __treeitem_add;
+	new->getparent = __treeitem_getparent;
+	new->getfirstchild = __treeitem_getfirstchild;
+	new->getnextchild = __treeitem_getnextchild;
+	new->takechild = __treeitem_takechild;
+	new->takename = __treeitem_takename;
+	new->getname = __treeitem_getname;
+	new->detach = __treeitem_detach;
+	new->delete = __treeitem_delete;
+	new->list.l_item.class.printattributes = 
+		(int(*)(PCLASS, int))(__treeitem_printattributes);
+	
 }
 /*
  * NAME		: newtreeitem
@@ -867,21 +1008,253 @@ static PTREE_ITEM __treeitem_getname (PTREE_ITEM root, char *name)
 struct tree_item * newtreeitem(struct tree_item *parent, char *name)
 {
 	PTREE_ITEM new = (PTREE_ITEM)calloc (1, sizeof (TREE_ITEM));
-	new->head = new->tail = new->curr = new->next = 0;
-	new->parent = parent;
-	list_resetlist (&new->list, name);
-	new->list.l_item.class.delete = (int(*)(PCLASS))(__treeitem_listdelete);
-	new->add = __treeitem_add; 
-	new->getparent = __treeitem_getparent;
-	new->getfirstchild = __treeitem_getfirstchild;
-	new->getnextchild = __treeitem_getnextchild;
-	new->takechild = __treeitem_takechild;
-	new->takename = __treeitem_takename;
-	new->getname = __treeitem_getname;
-	new->detach = __treeitem_detach;
-	new->delete = __treeitem_delete;
+	if (new)
+			__treeitem_ctor(new, parent, name);
 	
 	return new;
+}
+
+
+/**
+ * Name		: __circularitem_delete
+ * Description	: delete the supplied circular item
+ * 		  set data to null and free the data
+ **/
+void __circularitem_delete(PCIRCULARITEM item)
+{
+	item->next = item->prev = NULL; 
+	if (item->data)
+	{
+		memset (item->data, 0, item->datasize); 
+		free (item->data);
+	}
+	memset (item, 0 , sizeof (CIRCULARITEM));
+	free (item);
+}
+
+int  __circularitem_printattributes (PCIRCULARITEM citem, int ident)
+{
+	__class_printattributes (&citem->class, ident);
+	print_identation (ident);
+	fprintf (stdout, "prev : %p\n",  citem->prev);
+	print_identation (ident);
+	fprintf (stdout, "next : %p\n",  citem->next); 
+	return 0;
+	
+} 
+void __circularlist_add (PCIRCULARLIST clist, PCIRCULARITEM citem)
+{
+	if (!clist->current)
+	{
+		clist->current = citem;
+		citem->prev = citem->next = citem; 
+	}
+	else
+	{
+		if (clist->current->prev)
+		{
+			clist->current->prev->next = citem;
+			citem->next = clist->current;
+			citem->prev = clist->current->prev;
+			clist->current->prev = citem;
+		}
+	}
+	clist->count ++;
+}
+int __circularlist_printattributes (PCIRCULARLIST clist, int ident)
+{
+	PCIRCULARITEM citem; 
+	int k = clist->count;
+	__class_printattributes (&clist->class, ident);
+	citem = clist->current;
+	while (k)
+	{
+		citem->class.printattributes(&citem->class, ident+1);
+		citem = citem->next;	
+		k--;
+	}
+	return 0;	
+		
+}
+/**
+ * Name		: __circularitem_take 
+ * Description	: take current item from list 
+ *		  taken item is detached from list
+ */
+PCIRCULARITEM __circularlist_take (PCIRCULARLIST clist)
+{
+	PCIRCULARITEM citem = clist->current;
+	if (citem)
+	{
+		if (citem->prev)
+		{
+			citem->prev->next = citem->next; 
+		}
+		if (citem->next)
+		{
+			citem->next->prev = citem->prev;
+		}
+		if (citem->next == citem)
+		{ 
+			clist->current = NULL;		
+		}
+		else
+		{
+			clist->current = citem->next;
+		}
+		citem->next = citem->prev = NULL;
+		clist->count --; 
+		
+	}
+	return citem;
+	
+}
+
+/**
+ * Name		: __circularitem_takename 
+ * Description	: take an item of name from list
+ * Returns 	: the item if there is
+ *		  NULL others 
+ *
+ */
+PCIRCULARITEM  __circularlist_takename (PCIRCULARLIST clist, char *name)
+{
+	PCIRCULARITEM citem = clist->current;
+	int i = 0;
+	for (i = 0; i < clist->count ; i++)
+	{
+		if (!strcmp (citem->class.name, name) )
+		{
+			if (citem->prev)
+			{
+				citem->prev->next = citem->next;
+			}
+			if (citem->next)
+			{
+				citem->next->prev = citem->prev;
+			}
+			if (citem == clist->current)
+			{
+				if (citem->next == citem)
+					clist->current = NULL;
+				else
+					clist->current = citem->next;
+			}
+			return citem;
+		}
+		citem = citem->next;
+
+	}
+	return NULL;
+	
+}
+
+/*
+ * Name		: __circularlist_remove
+ * Description	: remove particular item from the list.
+ * Returns	  NULL if item is of this list
+ *		  Others if item is not of this list
+ */
+PCIRCULARITEM  __circularlist_remove (PCIRCULARLIST clist, PCIRCULARITEM citem)
+{
+	PCIRCULARITEM temp;
+	temp = clist->current;
+	if (!temp)
+		return citem;
+	do
+	{
+		if (citem == temp)
+		{
+			if (citem->prev)
+				citem->prev->next  = citem->next;
+			if (citem->next)
+				citem->next->prev  = citem->prev;
+			if (citem->next == citem)
+				clist->current = NULL;
+			else
+				clist->current = citem->next;
+			clist->count --;
+			citem->class.delete ((PCLASS)citem);
+			return NULL;
+		}
+		temp = temp->next;
+	}while (temp != clist->current);
+	return citem;
+}
+/**
+ * Name		: __circularlist_delete
+ * Description	: override class delete
+ */
+void __circularlist_delete (PCIRCULARLIST clist)
+{
+	while (clist->current)
+	{
+		__circularlist_remove (clist, clist->current);
+	}
+	clist->add = NULL;
+	clist->remove = NULL;
+	memset (clist, 0, sizeof (CIRCULARLIST));
+	free (clist); 
+} 
+
+void __circularitem_ctor (PCIRCULARITEM pciri, char *name, void *data, int size)
+{
+
+	pciri->class.this = (PCLASS) pciri;
+	pciri->class.type = CLASS_CIRCULARITEM;
+	strncpy (pciri->class.name, name, MAX_NAME_LENGTH - 1); 
+	pciri->next = pciri->prev = NULL;
+	pciri->class.delete = (int (*)(PCLASS))(__circularitem_delete);
+	pciri->class.printattributes = 
+		(int (*) (PCLASS, int))(__circularitem_printattributes);
+	pciri->datasize = size;
+	pciri->data = data; 
+}
+
+/**
+ * Name		: newcircularitem
+ * Description	: new circularitem
+ */
+
+PCIRCULARITEM newcircularitem (char *name, void *data, int size)
+{
+	PCIRCULARITEM pciri = (PCIRCULARITEM) calloc (1, sizeof (CIRCULARITEM));
+	if (pciri)
+	{
+		__circularitem_ctor (pciri, name, data, size);
+	}
+	return pciri;
+}
+void __circularlist_ctor (PCIRCULARLIST pcirlist, char *name)
+{
+	__class_ctor (&pcirlist->class, name); 
+	pcirlist->class.type = CLASS_CIRCULARLIST; 
+	pcirlist->add = (void (*) (PCIRCULARLIST, PCIRCULARITEM)) 
+			 (__circularlist_add);
+	pcirlist->remove = (int (*) (PCIRCULARLIST, PCIRCULARITEM)) 
+			 (__circularlist_remove);
+	pcirlist->take = (PCIRCULARITEM (*) (PCIRCULARLIST))
+			 (__circularlist_take);
+	pcirlist->takename = (PCIRCULARITEM (*) (PCIRCULARLIST, char *))
+			 (__circularlist_takename);
+	pcirlist->class.delete = (int (*)(PCLASS))(__circularlist_delete);
+	pcirlist->class.printattributes = 
+		(int (*) (PCLASS, int))(__circularlist_printattributes);
+
+}
+/**
+ * Name		: newcircularlist
+ * Description	: new circularlist
+ *
+ */
+PCIRCULARLIST newcircularlist (char *name)
+{
+	PCIRCULARLIST pcirlist = (PCIRCULARLIST) calloc (1, sizeof (CIRCULARLIST));
+	if (pcirlist)
+	{
+		__circularlist_ctor (pcirlist, name);
+	}
+	return pcirlist;
 }
 
 static int __primclass_delete (PPRIMCLASS p)
